ccecc1db733a5cc995249acb3b2b875e01432883
Cat <43008564+pemguin005@users.noreply.github.com>
2022-09-03 17:19:03 -0700

    Fixed keyboard/gamepad roation

--- NewCharacterBody3D.gd
+++ NewCharacterBody3D.gd
@@ -37,7 +37,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(0, 0, input_dir.y)).normalized()
 
 	if direction:
 		velocity.x = direction.x * SPEED
